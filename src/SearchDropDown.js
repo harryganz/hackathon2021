1010c276579bcd31ed29592dcc470d0847939c0c
Harry Ganz <hganz@leagueapps.com>
2021-10-27 23:15:51 -0400

    Styles Dropdown

--- src/SearchDropDown.js
+++ src/SearchDropDown.js
@@ -2,9 +2,17 @@ import { useRef } from "react";
 import SearchResult from "./SearchResult";
 import { ClickOutsideEffect } from "./utils";
 
+import "./SearchDropDown.css";
+
 function SearchDropDown({loading, results, showResults, setShowResults, addNewMonster}) {
     const searchDropDownRef = useRef(null);
     ClickOutsideEffect(searchDropDownRef, () => setShowResults(false))
+    
+    const addNewMonsterEffect = (monster) => {
+        addNewMonster(monster);
+        setShowResults(false);
+    }
+
     if (!showResults) {
         return '';
     }
@@ -14,7 +22,7 @@ function SearchDropDown({loading, results, showResults, setShowResults, addNewMo
     if (results.length === 0) {
         return (<div ref={searchDropDownRef} className="search-dropdown empty">No Results.</div>);
     }
-    let searchResults = results.map((el, i) => <SearchResult key={i} result={el} onClick={addNewMonster}/>);
+    let searchResults = results.map((el, i) => <SearchResult key={i} result={el} onClick={addNewMonsterEffect}/>);
     return (
         <div ref={searchDropDownRef} className="search-dropdown">
             <ul>{searchResults}</ul>
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,5 +1,11 @@
+import "./SearchResult.css";
 
 export default function SearchResult({result, onClick}) {
     let {name, challenge_rating} = result;
-    return (<li>Name: {name}, CR: {challenge_rating} <button className='add-monster' onClick={() => onClick(result)}>Add</button></li>);
+    return (
+        <li className='search-result'>
+            <span>{name} (CR {challenge_rating})</span>
+            <button className='btn add-monster' onClick={() => onClick(result)}>Add</button>
+        </li>
+    );
 }
